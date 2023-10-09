a99e52a8271933c859782af2102e982a0554a3a0
Sandro Campos <scampos@andrew.cmu.edu>
2023-10-09 11:05:28 -0400

    Fix module import for unit tests

--- tests/benchmarking_asv/test_example_module.py
+++ tests/benchmarking_asv/test_example_module.py
@@ -1,15 +1,15 @@
 """An example module containing testing of mock functions."""
 
-from benchmarking_asv import example_module
+import benchmarking_asv
 
 
 def test_greetings() -> None:
     """Verify the output of the `greetings` function"""
-    output = example_module.greetings()
+    output = benchmarking_asv.example_module.greetings()
     assert output == "Hello from LINCC-Frameworks!"
 
 
 def test_meaning() -> None:
     """Verify the output of the `meaning` function"""
-    output = example_module.meaning()
+    output = benchmarking_asv.example_module.meaning()
     assert output == 42
