4bbdb78587ee11afa728fe082dd1e61ea2aef33c
Sandro Campos <scampos@andrew.cmu.edu>
2024-01-29 16:19:14 -0500

    Update template version

--- src/benchmarking_asv/__init__.py
+++ src/benchmarking_asv/__init__.py
@@ -1,3 +1,3 @@
-from .example_module import greetings, meaning
+from .example_module import greetings, meaning, run_time_computation, run_mem_computation
 
-__all__ = ["greetings", "meaning"]
+__all__ = ["greetings", "meaning", "run_time_computation", "run_mem_computation"]
